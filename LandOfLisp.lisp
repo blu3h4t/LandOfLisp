4610e614eba12f70af29c53620067b3155ca037e
blu3h4t <bluehat@scarlet.be>
2016-08-08 14:41:15 +0200

    Update LandOfLisp.lisp

--- LandOfLisp.lisp
+++ LandOfLisp.lisp
@@ -100,12 +100,10 @@ there is a giant welding torch in the corner.))))
 
 (defun robots ()
 (loop named main
-with directions = '((a . -65) (z . -64) (e . -63)
-(q . -1) (s . 0) (d . 1)
-(w . 63) (x . 64) (c . 65))
-for pos = 544
-then (progn (format t "~%aze/qsd/wxc to move,
-(t)eleport, (l)eave:")
+        with directions = '((q . -65) (w . -64) (e . -63) (a . -1)
+                            (d .   1) (z .  63) (x .  64) (c . 65))
+        for pos = 544
+then (progn (format t "~%qwe/asd/zxc to move, (t)eleport, (l)eave:")
 (force-output)
 (let* ((c (read))
 (d (assoc c directions)))
